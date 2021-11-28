4f51f88999db0600701141a36c162511238d661e
Rehman <shaik.rehman07@gmail.com>
2021-11-27 19:04:11 -0500

    code changes to make total no. of pages visible

--- service.py
+++ service.py
@@ -4,25 +4,33 @@ import sys
 
 from zendesk import getAllTicketswithPaging, getTicketById
 
+def parsingFunction(data):
+    """
+    this function takes data from api which includes all fields and parse into the string which is easy to read.
+    """
+    #Extracting data from raw api endpoint data we get from zendest api for ticket
+    id = data["id"]
+    subject = data["subject"]
+    requester_id = data["requester_id"]
+    created_at = data["created_at"]
+    status = data["status"]
+    #Formatting the date to be read easily by others in "date Month(shortest form) Year" format
+    created_date = datetime.fromisoformat(created_at.replace("Z",""))
+    format_date = created_date.strftime("%d %b %Y")
+    #Simple string shown to end user after formatting the raw data
+    parsedData = f"Ticket with id '{id}' is opened by '{requester_id}' with subject '{subject}' on '{format_date}' with status '{status}'"
+    return parsedData
+
 def parseSingleTicket(ticket_id):
     """
-    this function helps to parse the details of ticket by id coming from zendesk api as per programmer requirement
+    this function helps to parse the details of ticket by id coming from zendesk api and handles any exceptions if present.
     returns: single parsed ticket data
     """
     parsedData = ""
     try:
         # get ticket details from zendesk api
         data = getTicketById(ticket_id)
-        #Extracting data from raw api endpoint data we get from zendest api for ticket by id
-        subject = data["ticket"]["subject"]
-        requester_id = data["ticket"]["requester_id"]
-        created_at = data["ticket"]["created_at"]
-        status = data["ticket"]["status"]
-        #Formatting the date to be read easily by others in "date Month(shortest form) Year" format
-        created_date = datetime.fromisoformat(created_at.replace("Z",""))
-        format_date = created_date.strftime("%d %b %Y")
-        #Simple string shown to end user after formatting the raw data
-        parsedData = f"Ticket is opened by '{requester_id}' with subject '{subject}' on '{format_date}' with status '{status}'"
+        parsedData = parsingFunction(data)
     except ValueNotFoundException:
         print(f"value with id {ticket_id} is not found.....")
     except AuthorizeException:
@@ -32,61 +40,66 @@ def parseSingleTicket(ticket_id):
         print("Something went wrong.....Please check url and credentials once...")
         sys.exit()
     return parsedData
-    
-def parseMultipleTickets(page):
-    """
-    this function helps to parse the details of mutliple tickets coming from zendesk api with given page as per programmer requirement
-    returns: list of parsed tickets, next page details, previous page details
-    """
-    full_parsed_data = []
-    next = ""
-    previous = ""
-    try:
-        # get tickets details from zendesk api only if atleast 1 ticket is present else return empty list of tickets
-        data = getAllTicketswithPaging(page)
-        if(len(data["tickets"]) > 0):
-            for i in range(0,len(data["tickets"])):
-                #Extracting data from raw api endpoint data we get from zendest api for tickets
-                id = data["tickets"][i]["id"]
-                subject = data["tickets"][i]["subject"]
-                requester_id = data["tickets"][i]["requester_id"]
-                created_at = data["tickets"][i]["created_at"]
-                status = data["tickets"][i]["status"]
-                #Formatting the date to be read easily by others in "date Month(shortest form) Year" format
-                created_date = datetime.fromisoformat(created_at.replace("Z",""))
-                format_date = created_date.strftime("%d %b %Y")
-                #Simple string shown to end user after formatting the raw data
-                parsedData = f"Ticket {id} is opened by '{requester_id}' with subject '{subject}' on '{format_date}' with status '{status}'"
-                full_parsed_data.append(parsedData)
-                #next page details
-                next = data["next_page"]
-                #previous page details
-                previous = data["previous_page"]
-    except AuthorizeException:
-        print("Could not authorize you.....Please check credentials")
-        sys.exit()
-    except Exception:
-        print("Something went wrong.....Please check url and credentials once...")
-        sys.exit()
-    return full_parsed_data, next, previous
 
 def printSingleTicket(ticket_id):
     """
     this function just prints the data received from below used function "parseSingleTicket"
     """
     data = parseSingleTicket(ticket_id)
-    print("\n" + data, end = "\n\n")
+    print("\n" + data, end = "\n")
+    
+def parseMultipleTickets(page):
+    """
+    this function helps to parse the details of mutliple tickets coming from zendesk api with given page limit
+    returns: list of parsed tickets, total tickets count in all pages, next page details, previous page details, limit per page
+    """
+    #list of parsed tickets with limit given as requirement i.e., 25
+    full_parsed_data = []
+    #next page details
+    next = ""
+    #previous page details
+    previous = ""
+    #total tickets count in all pages
+    total_tickets_count = 0
+    try:
+        data, count, next_page, prev_page, limit_per_page = getAllTicketswithPaging(page)
+        # get tickets details from zendesk api only if atleast 1 ticket is present else return empty list of tickets
+        if(len(data) > 0):
+            for i in range(0,len(data)):
+                #Extracting data from raw api endpoint data we get from zendest api for tickets
+                parsedData = parsingFunction(data[i])
+                full_parsed_data.append(parsedData)
+                #next page details
+                next = next_page
+                #previous page details
+                previous = prev_page
+                #get all tickets count
+                total_tickets_count = count
+    except AuthorizeException:
+        print("Could not authorize you.....Please check credentials")
+        sys.exit()
+    except Exception:
+        print("Something went wrong.....Please check url and credentials once...")
+        sys.exit()
+    return full_parsed_data, total_tickets_count, next, previous, limit_per_page
 
 def printMultipleticketswithPaging(page):
     """
     this function just prints the data received from below used function "parseMultipleTickets"
     input param : taking input param as "page" to display content of particular page if more than 25 tickets are returned from api
     """
-    data, next_page, previous_page = parseMultipleTickets(page)
+    data, count, next_page, previous_page, limit_per_page = parseMultipleTickets(page)
     # if data is present then only we need to print otherwise no data
     if(len(data) > 0):
+        #to count total number of pages with limit 25 per page
+        if(count%limit_per_page == 0):
+            total_pages = count/limit_per_page;
+        else:
+            total_pages = int(count/limit_per_page) + 1;
+        #print the data in 25 and page label with next and prev page details
         for i in range(0,len(data)):
             print("\n" + data[i])
+        print(f"\n========== Page {page} of {total_pages} ==========")
         print(f"\nNext page = {next_page}\nPrevious page = {previous_page}")
         #Based on next page and prev page details multiple options are made available to end user to procedd further or quit or go to previous list
         if next_page != None and previous_page == None:
@@ -98,6 +111,7 @@ def printMultipleticketswithPaging(page):
             #funtion that handles the inputs given by the user when this method call happens. Inputs should be entered as it suggested by above print statement in cli 
             inputs_for_next_prev_quit_for_pages(page, next_page, previous_page)
         elif previous_page != None and next_page == None:
+            print("\n\tAll tickets printed.....")
             print("\nType 'prev' for previous 25 Tickets or 'q' to quit")
             #funtion that handles the inputs given by the user when this method call happens. Inputs should be entered as it suggested by above print statement  in cli 
             inputs_for_next_prev_quit_for_pages(page, next_page, previous_page)
@@ -107,6 +121,9 @@ def printMultipleticketswithPaging(page):
         print("No data to print..")
 
 def inputs_for_next_prev_quit_for_pages(page, next_page, previous_page):
+    """
+    this function just asks input for next, prev or quit
+    """
     option = input("Enter your option: ")
     if option == "next" and next_page != None:
         printMultipleticketswithPaging(page+1)
--- zendesk.py
+++ zendesk.py
@@ -29,10 +29,12 @@ def getAllTicketswithPaging(page = 1):
     function returns raw data collected from url endpoint passed with page and limit per page details
     """
     per_page = 25
-    limit_per_page = url + '?page=' + str(page) + '&per_page=' + str(per_page) 
+    limit_per_page_url = url + '?page=' + str(page) + '&per_page=' + str(per_page) 
     #passing url for all tickets with paging with email and password to fetch the data
-    all_tickets = getData(limit_per_page, credentials.email, credentials.password)
-    return all_tickets
+    all_tickets = getData(limit_per_page_url, credentials.email, credentials.password)
+    #getting all details such as tickets in list, total tickets count, next page details, prev page details which are used for parsing as per page
+    tickets_list, count, next_page, prev_page = all_tickets["tickets"], all_tickets["count"], all_tickets["next_page"], all_tickets["previous_page"]
+    return tickets_list, count, next_page, prev_page, per_page
     
 def getTicketById(ticket_id : int):
     """
@@ -41,4 +43,6 @@ def getTicketById(ticket_id : int):
     final_url = url + str(ticket_id)
     #passing url for ticket by id with email and password to fetch the data 
     ticket_by_id = getData(final_url, credentials.email, credentials.password)
-    return ticket_by_id
+    #getting only ticket detail for parsing
+    ticket_details = ticket_by_id["ticket"]
+    return ticket_details
